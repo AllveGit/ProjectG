62ae2734d0eb29e1dd6607640a181f8db5ec24c5
JeongTaeLee <jeongtae8633@gmail.com>
2019-05-19 22:42:23 +0900

    생성된 방에 방장이 아닌사람 접속이 되면 OnJoinedRandom이 호출되지 않는 문제 해결 테스트 - lobby를 이용해봄

--- Assets/Script/Launcher.cs
+++ Assets/Script/Launcher.cs
@@ -33,63 +33,42 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         // 자동 동기화.
         PhotonNetwork.AutomaticallySyncScene = true;
-    
      }
 
     void Start()
     {
-        progressLabel.SetActive(false);
-        controlPanel.SetActive(true);
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
 
-    }
-
-
-    /// <summary>
-    /// 포톤 클라우드와 연결되는 시작점입니다.
-    /// </summary>
-    public void Connect()
-    {
-        isConnected = true;
-
-        if (PhotonNetwork.IsConnected)
-        {
-            PhotonNetwork.JoinRandomRoom();
-        }
-        else
+        if (!PhotonNetwork.IsConnected)
         {
             PhotonNetwork.GameVersion = strGameVersion;
             PhotonNetwork.ConnectUsingSettings();
         }
-
-        progressLabel.SetActive(true);
-        controlPanel.SetActive(false);
     }
 
+    #region PhotonCallBack
     public override void OnConnectedToMaster()
     {
         Debug.Log("마스터 서버에 연결되었습니다.");
-
-        if (isConnected)
-            PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinLobby();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        Debug.LogWarningFormat("연결 종료되었습니다.", cause);
 
         progressLabel.SetActive(false);
         controlPanel.SetActive(true);
     }
 
-    // Room 참가에 실패했을시 호출되는 Callback함수입니다.
-    public override void OnJoinRandomFailed(short returnCode, string message)
+    public override void OnJoinedLobby()
     {
-        Debug.Log("랜덤 방 참가에 실패하여 OnJoinRandomRoom 호출되었습니다.");
-
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = maxPlayersPerRooom;
-        PhotonNetwork.CreateRoom(null, roomOptions);
+        Debug.Log("로비에 연결되었습니다.");
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
     }
+
     public override void OnJoinedRoom()
     {
         Debug.Log("랜덤 방 참가에 성공하여 OnJoinedRoom 호출되었습니다.");
@@ -99,6 +78,27 @@ public class Launcher : MonoBehaviourPunCallbacks
             Debug.Log("1인용 맵을 로드합니다.");
             PhotonNetwork.LoadLevel("RoomFor1");
         }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("랜덤 방 참가에 실패하여 OnJoinRandomRoom 호출되었습니다.");
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = maxPlayersPerRooom;
+        PhotonNetwork.CreateRoom(null, roomOptions);
+    }
+    #endregion
+
+    public void Connect()
+    {
+        isConnected = true;
+
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+
+        if (isConnected)
+            PhotonNetwork.JoinRandomRoom();
     }
 
 }
