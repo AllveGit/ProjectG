425e91d8750932f240becfe0f4f0884bb3ef0857
JeongTaeLee <jeongtae8633@gmail.com>
2019-05-20 03:10:22 +0900

    플레이어 카메라 추가했음 맵 3 짧고 길게 바꿈

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -36,23 +36,38 @@ public class GameManager : MonoBehaviourPunCallbacks
         }
     }
 
-    #region Photon Callbacks
-    public override void OnLeftRoom()
+    #region Public Methods
+    public void LeaveRoom()
     {
-        SceneManager.LoadScene(0);
+        //방에서 나갑니다.
+        PhotonNetwork.LeaveRoom();
     }
 
+    //private void LoadArena()
+    //{
+    //    // 맵을 생성하는 것은 마스터 클라이언트의 일이기 때문에 마스터클라이언트인지 체크한다.
+    //    if (!PhotonNetwork.IsMasterClient)
+    //        Debug.LogError("PhotonNetwork : 우리는 마스터 클라이언트가 아닙니다.");
+    //
+    //    Debug.LogFormat("래밸이 로드되었습니다. : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+    //    /*해당 레벨을 로드합ㄴ이다.  PhotonNetwork.automaticallySyncScene 을 사용하도록 해놓았기 때문에 룸 안의 모든 접속한 클라이언트에 
+    //    대해 이 레벨 로드를 유니티가 직접하는 것이 아닌 Photon이 하도록 하였습니다.*/
+    //    PhotonNetwork.LoadLevel("RoomFor" + PhotonNetwork.CurrentRoom.PlayerCount);
+    //}  
+    #endregion
+
+    #region Photon
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         // 다른 player가 접속될때 현재 방인원수의 따라 새로 맵이 로드됩니다.
         Debug.LogFormat("다른 플레이어가 참가하였습니다() {0}", newPlayer.NickName);
 
         // 자신이 마스터 클라이언트일 경우만(방장)
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogFormat("방장이 방에 참가하였습니다.{0}", PhotonNetwork.IsMasterClient);
-            LoadArena();
-        }
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+        //    Debug.LogFormat("방장이 방에 참가하였습니다.{0}", PhotonNetwork.IsMasterClient);
+        //    LoadArena();
+        //}
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
@@ -60,35 +75,16 @@ public class GameManager : MonoBehaviourPunCallbacks
         Debug.LogFormat("다른 플레이어가 나갔습니다() {0}", otherPlayer.NickName);
 
         // 자신이 마스터 클라이언트일 경우만(방장)
-        if (PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogFormat("방장이 방에서 나갔습니다.{0}", PhotonNetwork.IsMasterClient);
-            LoadArena();
-        }
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+        //    Debug.LogFormat("방장이 방에서 나갔습니다.{0}", PhotonNetwork.IsMasterClient);
+        //    LoadArena();
+        //}
     }
 
-    #endregion
-
-    #region Public Methods
-    public void LeaveRoom()
+    public override void OnLeftRoom()
     {
-        //방에서 나갑니다.
-        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene(0);
     }
     #endregion
-
-    #region Private Methods
-    private void LoadArena()
-    {
-        // 맵을 생성하는 것은 마스터 클라이언트의 일이기 때문에 마스터클라이언트인지 체크한다.
-        if (!PhotonNetwork.IsMasterClient)
-            Debug.LogError("PhotonNetwork : 우리는 마스터 클라이언트가 아닙니다.");
-       
-        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-        /*해당 레벨을 로드합ㄴ이다.  PhotonNetwork.automaticallySyncScene 을 사용하도록 해놓았기 때문에 룸 안의 모든 접속한 클라이언트에 
-        대해 이 레벨 로드를 유니티가 직접하는 것이 아닌 Photon이 하도록 하였습니다.*/
-        PhotonNetwork.LoadLevel("RoomFor" + PhotonNetwork.CurrentRoom.PlayerCount);
-    }
-    #endregion
-
 }
---
+++ Assets/Script/PlayerWeapon/PlayerBeam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerBeam : MonoBehaviour
+{
+    private float fSpeed { get; set; } = 20f;
+    private float fRangeDistance { get; set; } = 100f;
+    private float fMoveDistance = 0;
+    private void Update()
+    {
+        if (fRangeDistance <= fMoveDistance)
+            PhotonNetwork.Destroy(this.gameObject);
+
+        Vector3 vDirection = transform.forward;
+        vDirection.y = 0;
+
+        float fDistance = fSpeed * Time.deltaTime;
+
+        transform.position += vDirection * fDistance;
+
+        fMoveDistance += fDistance;
+    }
+}
