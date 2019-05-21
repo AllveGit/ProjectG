4412568d5083ec0e9daeebd814fac1a65ca0cfed
Allve <createpubpriv@gmail.com>
2019-05-22 00:36:27 +0900

    맵툴 파싱 빼고 추가

---
+++ Assets/Editor/MapTool/CreateGrid.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateGrid : EditorWindow
+{
+    int Width = 0;
+    int Height = 0;
+
+    [MenuItem("MapTool/CreateGrid")]
+    static void OpenWindow()
+    {
+        CreateGrid window = (CreateGrid)GetWindow(typeof(CreateGrid));
+        window.minSize = new Vector2(250, 200);
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        RenderGUI();        
+    }
+
+    //맵툴 GUI 렌더
+    void RenderGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Width ");
+        Width = EditorGUILayout.IntField(Width);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Grid Height");
+        Height = EditorGUILayout.IntField(Height);
+        EditorGUILayout.EndHorizontal();
+
+        if(GUILayout.Button("Create", GUILayout.Height(40)))
+        {
+            FindObjectOfType<GridManager>().SettingGrid(Width, Height);
+        }
+    }
+}
