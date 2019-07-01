c213974b6cbfe21c4df40e9d940610dbb635f8b6
wisemin01 <wisemin01@naver.com>
2019-07-02 00:12:59 +0900

    메인 화면 UI 작업중

--- Assets/Script/Launcher/CharacterSelecter.cs
+++ Assets/Script/Launcher/CharacterSelecter.cs
@@ -13,11 +13,19 @@ public class CharacterSelecter : MonoBehaviour
     // 캐릭터 2D 이미지 뷰어
     public GameObject characterView;
 
+    // 배경 뷰어
+    public GameObject backgroundView;
+
     // 이미지 배열
     public Sprite[] characterSprites;
 
+    // 배경 배열 ( 배경은 캐릭터에 따라 다르게 바뀜 )
+    public Sprite[] backgroundSprites;
+
     // 현재 선택된 인덱스
+    [SerializeField]
     private Enums.CharacterIndex currentIndex = 0;
+    [SerializeField]
     private Enums.CharacterIndex maxIndex = 0;
 
     void Start() 
@@ -75,5 +83,10 @@ public class CharacterSelecter : MonoBehaviour
 
         characterView.GetComponent<UnityEngine.UI.Image>().sprite = characterSprites[(int)currentIndex];
         photonLauncher.characterType = currentIndex;
+
+        if (backgroundSprites[(int)currentIndex] == null)
+            return;
+
+        backgroundView.GetComponent<UnityEngine.UI.Image>().sprite = backgroundSprites[(int)currentIndex];
     }
 }
---
+++ Assets/Script/Launcher/UI Script/BaseWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWindow : MonoBehaviour
+{
+    public float restoreSpeed = 0.15f;
+    public Vector3 endScale;
+
+    private void ScaleRestore()
+    {
+        
+    }
+
+    public virtual void CloseWindow()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public virtual void ShowWindow()
+    {
+        if (gameObject.activeSelf == true)
+            return;
+
+        transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        gameObject.SetActive(true);
+    }
+
+    private void Start()
+    {
+        CloseWindow();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, endScale, restoreSpeed);
+    }
+}
