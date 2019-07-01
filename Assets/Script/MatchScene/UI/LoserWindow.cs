b4531a0f1a330fac0efddfb7f619b15d375d1c9c
wisemin01 <wisemin01@naver.com>
2019-07-02 05:56:10 +0900

    승리 패배 윈도우 생성

---
+++ Assets/Script/MatchScene/UI/LoserWindow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LoserWindow : BaseWindow
+{
+    public override void CloseWindow()
+    {
+        base.CloseWindow();
+    }
+
+    public override void ShowWindow()
+    {
+        base.ShowWindow();
+
+        StartCoroutine(RoomQuitCoroutine(3.0f));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseWindow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator RoomQuitCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        PhotonNetwork.LeaveRoom();
+
+        yield break;
+    }
+}
