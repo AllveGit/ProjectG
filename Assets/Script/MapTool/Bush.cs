7cfc5c4819e09fc7149ddf3d8828cb4545699f30
AllveGit <createpubpriv@gmail.com>
2019-07-02 03:13:58 +0900

    부쉬 투명화 처리 완료

---
+++ Assets/Script/MapTool/Bush.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    public Renderer m_Mat;
+
+    private void Start()
+    {
+        m_Mat = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Bush")
+        {
+            m_Mat.material.SetFloat("Vector1_3BE6800", 0.2f); //Alpha
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Bush")
+        {
+            m_Mat.material.SetFloat("Vector1_3BE6800", 1f); //Alpha
+        }
+    }
+}
