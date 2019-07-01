31a9e077a29e90dc93497864e03822731555f736
JeongTaeLee <jeongtae8633@gmail.com>
2019-07-01 10:02:11 +0900

    여러가지 버그 수정 1. 새로추가한 캐릭터의 태그 설정. 2.Connecting을 알리는 화면 추가

--- Assets/Script/MatchScene/Player/Healer.cs
+++ Assets/Script/MatchScene/Player/Healer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Photon.Pun;
+
 public class Healer : BasePlayer
 {
     void Start()
@@ -20,7 +22,25 @@ public class Healer : BasePlayer
 
     public override void Attack()
     {
-        if (!photonView.IsMine) return;
+        if (photonView.IsMine == false)
+            return;
+
+        animator.SetBool("Attack", true);
+
+        StartCoroutine(DelayAttack(delegate (Vector3 direction)
+        {
+            if (direction == Vector3.zero)
+                return;
+
+            GameObject projectile = PhotonNetwork.Instantiate(
+                "Skill/" + basicAttackPrefab.name,
+                transform.position + transform.forward + new Vector3(0, 0.5f, 0),
+                transform.rotation);
+
+            if (projectile != null)
+                projectile.GetComponent<Arrow>().Cast(this, AttackDamage, direction);
+
+        }, SkillJoyStick.Amount, 0.6f));
 
     }
     public override void UltimateSkill()
