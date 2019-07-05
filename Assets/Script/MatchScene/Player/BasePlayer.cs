7e8284e5f451823b7e82b491dcaaf23b63882069
JeongTaeLee <jeongtae8633@gmail.com>
2019-07-05 16:27:07 +0900

    리스폰 추가, 플레이어 위치 오류 해결

--- Assets/Script/MatchScene/Player/BasePlayer.cs
+++ Assets/Script/MatchScene/Player/BasePlayer.cs
@@ -18,6 +18,9 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
         animator        = GetComponent<Animator>();
         if (animator == null)
             Debug.LogError("BasePlayer.cs / animator를 가져오지 못했습니다.");
+        collider = GetComponent<CapsuleCollider>();
+        if (collider == null)
+            Debug.LogError("BasePlayer.cs / collider를 가져오지 못했습니다.");
 
         if (photonView.IsMine)
         {
@@ -31,9 +34,17 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
         if (SkillJoyStick == null)
             Debug.LogError("BasePlayer.cs / SkillJoyStick을 가져오지 못했습니다.");
 
+
         // 이벤트 핸들러에 등록
         SkillJoyStick.OnStickUp     += OnSkillJoyStickUp;
         SkillJoyStick.OnStickDown   += OnSkillJoyStickDown;
+    }
+    public void PlayerInit(Enums.TeamOption team, Vector3 pos)
+    {
+        transform.position = pos;
+        playerTeam = team;
+
+        photonView.RPC("RPCTranslatePosition", RpcTarget.Others, transform.position);
     }
 
     protected void FixedUpdate()
@@ -45,14 +56,21 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
         }
         else
         {
+            if (Vector3.Distance(transform.position, currentPosition) > 3)
+            {
+                transform.position = currentPosition;
+                photonView.RPC("RPCTranslatePosition", RpcTarget.Others, transform.position);
+            }
+
             transform.position = Vector3.MoveTowards(transform.position, currentPosition, moveSpeed * Time.deltaTime);
             transform.rotation = currentRotation;
         }
     }
 
+
     public void MoveCalculate()
     {
-        if (animator.GetBool("Attack") == true)
+        if (animator.GetBool("Attack") || animator.GetBool("Death"))
         {
             movementAmount = Vector3.zero;
             animator.SetFloat("Speed", 0f);
@@ -60,10 +78,6 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
             return;
         }
 
-        // 키보드
-        // float v = Input.GetAxis("Vertical"); // 수직
-        // float h = Input.GetAxis("Horizontal"); // 수평
-        // movementAmount = new Vector3(h, 0f, v).normalized * moveSpeed;
 
         movementAmount = MoveJoyStick.JoyDir * (moveSpeed * MoveJoyStick.JoyScale) * Time.deltaTime;
         animator.SetFloat("Speed", movementAmount.magnitude * 10);
@@ -73,9 +87,10 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
 
     public virtual void RotateCalculate()
     {
-        if (animator.GetBool("Attack") == true) return;
+        if (animator.GetBool("Attack") || animator.GetBool("Death")) return;
 
-        rigidbody.rotation = Quaternion.LookRotation(movementAmount);
+        if (movementAmount != Vector3.zero)
+            rigidbody.rotation = Quaternion.LookRotation(movementAmount);
     }
 
     private void AttackBehavior()
@@ -101,18 +116,6 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
 
         UltimateSkill();
     }
-
-    public void OnSkillJoyStickUp(Vector3 pos, Vector3 dir)
-    {
-        AttackBehavior();
-        isFocusOnAttack = false;
-    }
-
-    public void OnSkillJoyStickDown(Vector3 pos, Vector3 dir)
-    {
-        isFocusOnAttack = true;
-    }
-
     public void OnDamaged(int damage)
     {
         photonView.RPC("RPCOnDamage", RpcTarget.Others, damage);
@@ -122,12 +125,21 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
     {
         photonView.RPC("RPCOnHeal", RpcTarget.Others, heal);
     }
-
+    public void OnSkillJoyStickUp(Vector3 pos, Vector3 dir)
+    {
+        AttackBehavior();
+        isFocusOnAttack = false;
+    }
+    public void OnSkillJoyStickDown(Vector3 pos, Vector3 dir)
+    {
+        isFocusOnAttack = true;
+    }
     public abstract void Attack();          // 기본공격을 사용하기 위한 함수
     public abstract void UltimateSkill();   // 궁극기 스킬을 사용하기 위한 함수
     public virtual void OnPlayerDeath()   // 플레이어가 죽을 때 호출됨
     {
-        if (!photonView.IsMine) return;
+        if (animator.GetBool("Death") == true)
+            return;
 
         ExitGames.Client.Photon.Hashtable oldHashTable = PhotonNetwork.CurrentRoom.CustomProperties;
         ExitGames.Client.Photon.Hashtable newHashTable = new ExitGames.Client.Photon.Hashtable();
@@ -147,8 +159,11 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
         }
         PhotonNetwork.CurrentRoom.SetCustomProperties(newHashTable);
 
+        collider.enabled = false;
+        rigidbody.useGravity = false;
+
         animator.SetBool("Death", true);
-        //StartCoroutine(Respawn(5f));
+        StartCoroutine(Respawn(5f));
     }
 
     public IEnumerator DelayAttack(AttackCallback attackCallback, Vector3 direction, float delay)
@@ -160,11 +175,16 @@ public abstract partial class BasePlayer : MonoBehaviourPun, IPunObservable
     public IEnumerator Respawn(float delay)
     {
         yield return new WaitForSeconds(delay);
-        //GameManager.Instance.Respawn(this);
         animator.SetBool("Death", false);
 
         CurHP = MaxHP;
         ShieldPower = MaxShieldPower;
+
+        transform.position = GameManager.Instance.GetRespawnPos();
+        photonView.RPC("RPCTranslatePosition", RpcTarget.Others, transform.position);
+
+        collider.enabled = true;
+        rigidbody.useGravity = true;
 
         yield break;
     }
@@ -215,10 +235,9 @@ public abstract partial class BasePlayer
                 ShieldPower = 0;
             }
 
-            if (CurHP < 0)
+            if (CurHP <= 0)
             {
                 CurHP = 0;
-
                 OnPlayerDeath();
             }
         }
@@ -233,6 +252,12 @@ public abstract partial class BasePlayer
             if (CurHP > maxHP)
                 CurHP = maxHP;
         }
+    }
+    [PunRPC]
+    protected virtual void RPCTranslatePosition(Vector3 translationPos)
+    {
+        currentPosition = translationPos; 
+        transform.position = translationPos;
     }
 
 }
@@ -273,9 +298,6 @@ public abstract partial class BasePlayer
     private float moveSpeed = 0; // 플레이어의 이동 속도
 
     [SerializeField]
-    private float attackSpeed = 0; // 플레이어의 공격 속도
-
-    [SerializeField]
     private int attackDamage = 0; // 플레이어의 공격력
 
     [SerializeField]
@@ -284,9 +306,6 @@ public abstract partial class BasePlayer
     [SerializeField]
     private int maxShieldPower = 0; // 플레이어의 쉴드(추가 체력) 최대치
 
-    [SerializeField]
-    [Range(0.0f, 1.0f)]
-    private float rotationLerpSpeed = 0f; // 플레이어의 회전 보간 속도
 }
 
 /*
@@ -296,6 +315,7 @@ public abstract partial class BasePlayer
 {
     public new Rigidbody rigidbody { get; private set; } = null;
     public Animator animator { get; private set; } = null;
+    public CapsuleCollider collider { get; private set; } = null;
     public PlayerCamera playerCamera { get; private set; } = null;
 
     public bool IsBush { get; set; } = false;
@@ -304,9 +324,7 @@ public abstract partial class BasePlayer
     public int CurHP { get => curHP; set => curHP = value; }
     public int MaxHP { get => maxHP; set => maxHP = value; }
     public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
-    public float AttackSpeed { get => attackSpeed; set => attackSpeed = value; }
     public int AttackDamage { get => attackDamage; set => attackDamage = value; }
     public int ShieldPower { get => shieldPower; set => shieldPower = value; }
     public int MaxShieldPower { get => maxShieldPower; set => maxShieldPower = value; }
-    public float RotationLerpSpeed { get => rotationLerpSpeed; set => rotationLerpSpeed = value; }
 }
