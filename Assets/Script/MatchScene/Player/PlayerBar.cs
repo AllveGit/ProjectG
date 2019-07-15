de76b7d74ea42c525919f8f07eb9fee200154ce6
wisemin01 <wisemin01@naver.com>
2019-07-16 01:26:31 +0900

    체력바 수정

--- Assets/Script/MatchScene/Player/PlayerBar.cs
+++ Assets/Script/MatchScene/Player/PlayerBar.cs
@@ -8,15 +8,21 @@ public class PlayerBar : MonoBehaviour
     private Sprite redTeamHpGaugeSprite;
     [SerializeField]
     private Sprite blueTeamHpGaugeSprite;
+    [SerializeField]
+    private Sprite backHpGaugeSprite;
 
     [SerializeField]
     private Image hpGauge;
+
+    [SerializeField]
+    private Image hpPrevGauge;
 
     [SerializeField]
     private Image manaGauge;
 
     float oneHpLength = 0f;
     float oneShieldLength = 0f;
+    float lerpSpeed = 0.1f;
 
     GameObject ownerObject = null;
 
@@ -26,13 +32,18 @@ public class PlayerBar : MonoBehaviour
         {
             transform.position = Camera.main.WorldToScreenPoint(ownerObject.transform.position);
             transform.Translate(new Vector3(0f, 80f, 0f));
-        }
 
+            float lerpWidth = Mathf.Lerp(hpPrevGauge.rectTransform.sizeDelta.x, hpGauge.rectTransform.sizeDelta.x, lerpSpeed);
+
+            hpPrevGauge.rectTransform.sizeDelta = new Vector2(lerpWidth, hpPrevGauge.rectTransform.sizeDelta.y);
+        }
     }
 
     public void BarInit(GameObject inOwnerObject, Enums.TeamOption teamOption, int maxHp, int maxShield)
     {
         hpGauge.sprite = (teamOption == Enums.TeamOption.BlueTeam) ? blueTeamHpGaugeSprite : redTeamHpGaugeSprite;
+        hpPrevGauge.sprite = backHpGaugeSprite;
+
         oneHpLength = hpGauge.sprite.rect.width / maxHp;
         oneShieldLength = manaGauge.sprite.rect.width / maxShield;
         ownerObject = inOwnerObject;
