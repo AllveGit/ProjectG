1e4ed7419a477b438d32457399ad49d0e15e3a08
AllveGit <createpubpriv@gmail.com>
2019-07-16 01:08:21 +0900

    디버그 코드 제거 - 카메라 셰이크

--- Assets/Script/MapTool/MapParser.cs
+++ Assets/Script/MapTool/MapParser.cs
@@ -35,10 +35,6 @@ public class MapParser : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(FindObjectOfType<PlayerCameraShake>().Shake(1f, 0.1f));
-        }
     }
 
     void Parsing()
